6150581f73206f55df68c09d75fa2e97ee1d4c1a
Gustavo S. Agapio <gsilvaagapio123@gmail.com>
2024-06-26 08:39:56 -0300

    Primeiros Exercícios do Curso da Udemy

---
+++ Programas/Outros Programas/Exercícios/Ex0001.py
@@ -0,0 +1,10 @@
+# Curso Udemy - Exercício 0001 - Operações Simples
+n = 5
+n2 = 3
+print("Divisão inteira: ", n // n2)
+print("Divisão comum: ", n / n2)
+print("Multiplicação: ", n * n2)
+print("Ao quadrado: ", n ** n2)
+print("Resto da divisão: ", n % n2)
+print("Soma: ", n + n2)
+print("Subtração: ", n - n2)
---
+++ Programas/Outros Programas/Exercícios/Ex0002.py
@@ -0,0 +1,9 @@
+# Curso Udemy - Exercício 0002 - Variaveis com função (simples)
+
+def estojo(numero):
+    x = numero
+    x = x ** 2
+
+    print("Numero calculado: ", x)
+
+estojo(10)
